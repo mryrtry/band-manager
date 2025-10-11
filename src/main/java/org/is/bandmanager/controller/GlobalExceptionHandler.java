79699edc7290ab6640ea1297afdae9050a92612e
mryrtry <maks01-16@mail.ru>
2025-10-11 18:26:32 +0300

    fix: add const fields order in GlobalExceptionHandler

--- src/main/java/org/is/bandmanager/controller/GlobalExceptionHandler.java
+++ src/main/java/org/is/bandmanager/controller/GlobalExceptionHandler.java
@@ -3,25 +3,31 @@ package org.is.bandmanager.controller;
 import org.is.bandmanager.exception.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.util.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static List<String> getFieldOrder(Class<?> clazz) {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .map(Field::getName)
+                .toList();
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new LinkedHashMap<>();
-        ex.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
+        List<String> order = getFieldOrder(Objects.requireNonNull(ex.getBindingResult().getTarget()).getClass());
+        ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .sorted(Comparator.comparingInt(e -> order.indexOf(e.getField())))
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
