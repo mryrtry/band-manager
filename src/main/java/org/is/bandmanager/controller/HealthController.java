90d6d5a98bf66e2794fd4b7c9060c056ac68306d
mryrtry <maks01-16@mail.ru>
2025-10-13 15:26:32 +0300

    feat: add HealthController

---
+++ src/main/java/org/is/bandmanager/controller/HealthController.java
@@ -0,0 +1,17 @@
+package org.is.bandmanager.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+
+}
