d6a7dd96355bf40b43153a678f5181d1488c04ca
mryrtry <maks01-16@mail.ru>
2025-10-14 22:09:02 +0300

    feat: add header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
 import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
