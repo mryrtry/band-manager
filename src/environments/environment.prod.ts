6164284d56029010d331896becc852c8d8c4b4e1
mryrtry <maks01-16@mail.ru>
2025-10-14 18:43:37 +0300

    feat: add pipeline to auto deploy (deploy.yml)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://se.ifmo.ru/~s409483/api',
+  version: '1.0.0',
+  enableDebug: false
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  version: '1.0.0-dev',
+  enableDebug: true
+};
