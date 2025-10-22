aecf28c2f1341dc391d2b4b4092b94d03018cb9d
mryrtry <maks01-16@mail.ru>
2025-10-22 17:40:57 +0300

    feat: update mapping structure, add event publishing

---
+++ src/main/java/org/is/bandmanager/dto/mapper/AlbumMapper.java
@@ -0,0 +1,20 @@
+package org.is.bandmanager.dto.mapper;
+
+import org.is.bandmanager.dto.AlbumDto;
+import org.is.bandmanager.dto.request.AlbumRequest;
+import org.is.bandmanager.model.Album;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface AlbumMapper {
+
+    AlbumDto toDto(Album album);
+
+    @Mapping(target = "id", ignore = true)
+    Album toEntity(AlbumRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromRequest(AlbumRequest request, @MappingTarget Album entity);
+
+}
