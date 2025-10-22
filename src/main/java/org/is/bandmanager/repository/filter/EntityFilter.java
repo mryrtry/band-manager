2dd802c730a07ad0479100d2dfeb9598345475e8
mryrtry <maks01-16@mail.ru>
2025-10-23 00:26:28 +0300

    fix: fix abstract class

--- src/main/java/org/is/bandmanager/repository/filter/EntityFilter.java
+++ src/main/java/org/is/bandmanager/repository/filter/EntityFilter.java
@@ -1,4 +1,12 @@
 package org.is.bandmanager.repository.filter;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = MusicBandFilter.class, name = "musicBand"),
+        @JsonSubTypes.Type(value = BestBandAwardFilter.class, name = "bestBandAward")
+})
 public interface EntityFilter {
 }
